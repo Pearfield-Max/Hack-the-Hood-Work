0d34ff79ebdaade81f0989e61ee26fb9d2ce24ad
Pearfield-Max <Jayla.Dunwood2@gmail.com>
2025-06-18 14:23:45 -0700

    addedd Lab2

---
+++ Lab2.py
@@ -0,0 +1,19 @@
+# Lab 2 Python Profiles 
+
+## Step 1
+name = "Jayla Duwnwood" 
+
+## step 2 
+nickname = "Jay"
+
+## step 3
+age = 17
+
+## step 4
+used_python = False
+
+## step 5 
+hobbies = ["drawing", "playing games", "reading manga","watching anime",]
+
+## step 6
+favorite_things = {"movie":"Scott Pilgrim vs the World", "game": "Mario and Zelda", "animal": "cat", "Dog","red panda"}

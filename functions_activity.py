38e1305caf32aee523acfdfceaced532ea0e8eaf
Pearfield-Max <Jayla.Dunwood2@gmail.com>
2025-06-29 21:03:42 -0700

    message

---
+++ functions_activity.py
@@ -0,0 +1,30 @@
+
+Menu = {"Pizza": 2.99, "Burger": 3.99, "HotDog": 1.99, "Cheese": 0.59, "IceCream": 1.49, "Churro": 0.79, "Soda": 0.89}
+def total_price(Burger, Soda): 
+     price = (3.99 + 0.89)
+     return price
+     print(total_price)
+
+
+def price_difference(Pizza, Churro):
+   num_complex = 2.99 - 0.79
+   abs_complex = abs(num_complex)
+   print(f"the difference of {2.99 - 0.79}" is {abs_complex})
+
+
+def inflation(Menu):
+     price = (1/2) * (Menu)
+     return price 
+     print(inflation)
+
+
+def deflation(Menu):
+     price = (Menu) / 2
+     return price
+
+
+
+
+
+
+

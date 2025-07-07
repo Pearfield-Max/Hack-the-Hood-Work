7607fc3d23343ecc6c5ff8eb8d5f3b8815ce4b71
Pearfield-Max <Jayla.Dunwood2@gmail.com>
2025-07-07 00:03:50 -0700

    Update crud_activity.py

--- crud_activity.py
+++ crud_activity.py
@@ -1,1 +1,65 @@
-As chief, i want apple so i can make pie 
+cookbook = []
+def create(recipe):
+    cookbook.append(recipe)
+    print(f"{recipe} has been added to the cookbook")
+
+def read(index):
+    cookbook(index)
+    if index in range(len(cookbook)):
+        return cookbook[index]
+
+def update(index, recipe):
+    if index in range(len(cookbook)):
+     print(f"{indext, recipe} recipe has been updated")
+
+def destroy(index):
+    cookbook.remove(index)
+    if index in range(len(cookbook)):
+        print(f"{recipe} recipe has been removed")
+
+def list_all_recipes( ):
+    cookbook.sort(recipe)
+    for i in cookbook(recipe):
+        print(i)
+
+def main():
+    while True:
+        print("\nCookbook Recipes")
+        print("1. Add a Recipe")
+        print("2. Read a Recipe")
+        print("3. Update a Recipe")
+        print("4. Delete a Recipe")
+        print("5. Display all Recipes")
+        print("6. Exit")
+
+
+        choice = input("Choose an option (1-6): ")
+
+
+        if choice == "1":
+            recipe = input("Enter the name of the recipe: ")
+            create(recipe)
+        elif choice == "2":
+            index = input("Enter the index of the recipe to read: ")
+            index = int(index)
+            read(index)
+        elif choice == "3":
+            index = input("Enter the index of the recipe to update: ")
+            recipe = input("Enter the name of the recipe you want to update it with: ")
+            index = int(index)
+            update(index, recipe)
+        elif choice == "4":
+            index = input("Enter the index of the recipe to delete: ")
+            index = int(index)
+            destroy(index)
+        elif choice == "5":
+            list_all_recipes()
+        elif choice == "6":
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+
+main()
+

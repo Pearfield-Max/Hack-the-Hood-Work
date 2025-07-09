8657a96a25b776849b385810290773dc81276cd0
Pearfield-Max <Jayla.Dunwood2@gmail.com>
2025-07-09 15:26:04 -0700

    Add files via upload

---
+++ debugging_act.py
@@ -0,0 +1,74 @@
+# code snippet 1 
+x = 10
+y = 5
+result = x / y
+print("Result:", result)
+
+#code snippet 2
+numbers = [1, 2, 3, 4, 5]
+for i in range(len(numbers)):
+    print(numbers[i])
+
+#code snippet 3
+def calculate_area(radius):
+   area = 3.14 * radius ** 2
+   return area
+radius = 5
+print(calculate_area(radius))
+
+#code snippet 4
+def is_even(number):
+   if number % 2 == 0:
+       return True
+   else:
+       return False
+ 
+print(is_even(4))
+print(is_even(7))
+
+#code snippet 5
+for i in range(5):
+    print(i)
+
+#code snippet 6
+def greet(name):
+    return "hello" + name
+print(greet( "Alice"))
+
+#code snippet 7
+numbers = [1, 2, 3, 4, 5,]
+sum = 9
+for number in numbers:
+    sum += number
+print("sum of numbers:, sum")
+
+#code snippet 8
+def factorial(n):
+    if n == 0:
+        return 1
+    else: 
+        return n * factorial(n-1)
+print(factorial(5))
+
+#code snippet 9
+name = Rose
+if name == "Alice" or name == "Bob":
+    print("hello, " + name)
+else:
+    print("hello, stranger!")
+
+#code snippet 10
+def divide_numbers(x, y):
+    if y == 0:
+        return " cant divide by zero!"
+else:
+     result = x / y 
+     return result
+num1 = 10
+num2 = 2
+print(divide_numbers(num1, num2))
+
+    
+
+
+
